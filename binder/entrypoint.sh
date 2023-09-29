737265fca27675b5aeb1bcd5f177f00a3fef2b90
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-09-29 16:29:42 +0200

    start xpra at startup

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,4 +5,6 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 roslaunch --wait pycram ik_and_description.launch &
 
+cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
+
 exec "$@"
