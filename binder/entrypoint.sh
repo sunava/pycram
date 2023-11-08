5fb9dd423edff1b2b92148c081eb8c588171715f
Kecks <philk99@web.de>
2023-11-08 15:20:07 +0100

    added rvizweb_config

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,7 +2,7 @@
 
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch &
+roslaunch --wait pycram rvizweb.launch &
 roslaunch --wait pycram ik_and_description.launch &
 
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
