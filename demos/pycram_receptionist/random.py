6de54d7678e96207e90718abb560a7be0a6e7a71
Celina SuturoLaptop <celina5@uni-bremen.de>
2024-01-19 14:31:40 +0100

    [Test file Receptionist] first connection to knowledge fix

--- demos/pycram_receptionist/random.py
+++ demos/pycram_receptionist/random.py
@@ -1,33 +1,38 @@
 import rospy
 from std_msgs.msg import String
+from std_srvs.srv import IsKnown
 
-from pycram.external_interfaces.knowrob import instances_of, get_guest_info
-import random
+#from pycram.external_interfaces.knowrob import instances_of, get_guest_info
+#import random
 
-def client_call(id):
-    rospy.init_node("client_node")
-    rospy.wait_for_service("serviceName")
-    rate = rospy.Rate(1)
-    while not rospy.s_shutdown():
-        try:
-            service_variable = rospy.ServiceProxy("SeviceName", Type)
-            respone = service_variable(parameterForService)
-            rate.sleep()
-        except rospy.ServiceExcepion as e:
-            print("Service call failed")
+#def client_call(id):
+#    rospy.init_node("client_node")
+#    rospy.wait_for_service("serviceName")
+#    rate = rospy.Rate(1)
+#    while not rospy.s_shutdown():
+#        try:
+#            service_variable = rospy.ServiceProxy("SeviceName", Type)
+#            respone = service_variable(parameterForService)
+#            rate.sleep()
+#        except rospy.ServiceExcepion as e:
+#            print("Service call failed")
 
 
-def get_guest_info(id: int):
+def get_guest_info(id):
     """
     function that uses Knowledge Service to get Name and drink from new guest via ID
     """
 
     #TODO: Service für Name und Getränk mit ID einbauen, wenn dieser fertig
-    rospy.wait_for_service('id_server')
+    rospy.wait_for_service('name_server')
+    print("debug")
     try:
         #TODO: Service class herausfinden
-        info_service = rospy.ServiceProxy('id_server', String)
+        print("in try")
+        info_service = rospy.ServiceProxy('name_server', IsKnown)
+        print("serverProxy")
         guest_data = info_service(id) #guest_data = List der Form ["name", "drink"]
+        print("end")
         return guest_data
     except rospy.ServiceException as e:
         print("Service call failed")
@@ -35,4 +40,4 @@ def get_guest_info(id: int):
 
 if __name__ == '__main__':
    print("starte test")
-   print(get_guest_info(2))
+   print(get_guest_info("4.0"))
