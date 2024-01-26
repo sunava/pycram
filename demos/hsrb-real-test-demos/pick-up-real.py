062843cd34268b4bd864020c94ecae27388b4c84
Celina SuturoLaptop <celina5@uni-bremen.de>
2024-01-26 16:26:34 +0100

    worked on picking up and place together with Mohammad

--- demos/hsrb-real-test-demos/pick-up-real.py
+++ demos/hsrb-real-test-demos/pick-up-real.py
@@ -36,19 +36,22 @@ v = VizMarkerPublisher()
 # Initialize Giskard interface for motion planning
 
 
-giskardpy.init_giskard_interface()
-#giskardpy.removing_of_objects()
-giskardpy.sync_worlds()
+
 #giskardpy.spawn_kitchen()
 # Create and configure the robot object
-robot = Object("hsrb", ObjectType.ROBOT, "../../resources/hsrb.urdf", pose=Pose([1, 2, 0]))
+robot = Object("hsrb", ObjectType.ROBOT, "../../resources/hsrb.urdf", pose=Pose([0, 0, 0]))
+# Update robot state
+RobotStateUpdater("/tf", "/giskard_joint_states")
+giskardpy.init_giskard_interface()
+#giskardpy.removing_of_objects()
+#giskardpy.sync_worlds()
 robot.set_color([0.5, 0.5, 0.9, 1])
 
 # Create environmental objects
-apartment = Object("kitchen", ObjectType.ENVIRONMENT, "test-room.urdf")
+apartment = Object("kitchen", ObjectType.ENVIRONMENT, "couch-kitchen.urdf")
 
 # Define orientation for objects
-object_orientation = axis_angle_to_quaternion([0, 0, 1], 0)
+object_orientation = axis_angle_to_quaternion([0, 0, 1], 180)
 
 # Define a breakfast cereal object
 #breakfast_cereal = Object("breakfast_cereal", "breakfast_cereal", "breakfast_cereal.stl", pose=Pose([4.8, 2.6, 0.87]),color=[0, 1, 0, 1])
@@ -61,52 +64,42 @@ object_orientation = axis_angle_to_quaternion([0, 0, 1], 0)
 # Set the world's gravity
 #world.set_gravity([0.0, 0.0, 9.81])
 
-# Update robot state
-RobotStateUpdater("/tf", "/giskard_joint_states")
 
 # Main interaction sequence with semi-real robot
-with real_robot:
+with ((real_robot)):
     rospy.loginfo("Starting demo")
     TalkingMotion("Starting demo").resolve().perform()
-
     MoveGripperMotion(motion="open", gripper="left").resolve().perform()
-    #
-    # #Initial setup actions
     ParkArmsAction([Arms.LEFT]).resolve().perform()
-    LookAtAction(targets=[Pose([3.8, 1.3, 0.33])]).resolve().perform()
-    #LookAtAction(targets=[Pose([3.8, 1.3, 0.33])]).resolve().perform()
-    # # Navigate to cereal location
     TalkingMotion("Navigating").resolve().perform()
-    #NavigateAction(target_locations=[Pose([4.1, 2, 0], object_orientation)]).resolve().perform()
+    NavigateAction(target_locations=[Pose([1.6, 1.8, 0], [0,0,1,0])]).resolve().perform()
+    #popcorntable
+    LookAtAction(targets=[Pose([0.8, 1.8, 0.21], object_orientation)]).resolve().perform()
+    LookAtAction(targets=[Pose([0.8, 1.8, 0.21], object_orientation)]).resolve().perform()
+    TalkingMotion("Perceiving").resolve().perform()
     object_desig = DetectAction(technique='default').resolve().perform()
     object_dict = object_desig[1]
-    one = False
-    #todo only pick up when on table
+
     for key, value in object_dict.items():
-        #if not one:
-        if object_dict[key].type == "Bowl":
-            TalkingMotion("Picking Up").resolve().perform()
+        if (object_dict[key].type == "Spoon"
+                or object_dict[key].type == "Fork"
+                or object_dict[key].type == "Knife"
+                or object_dict[key].type == "Plasticknife"):
+            object_dict[key].type = "Cutlery"
             grasp = "top"
+            TalkingMotion("Picking Up with: " + grasp).resolve().perform()
             PickUpAction(object_dict[key], ["left"], [grasp]).resolve().perform()
             ParkArmsAction([Arms.LEFT]).resolve().perform()
-            NavigateAction(target_locations=[Pose([4.1, 2, 0], object_orientation)]).resolve().perform()
-
-
-            PlaceAction(object_dict[key], ["left"], [grasp], [Pose([4.8, 2.6, 0.80])]).resolve().perform()
+            TalkingMotion("Navigating").resolve().perform()
+            NavigateAction(target_locations=[Pose([1.6, 1.8, 0], [0,0,0,1])]).resolve().perform()
+            NavigateAction(target_locations=[Pose([4.1, 2, 0], [0, 0, 0, 1])]).resolve().perform()
+            TalkingMotion("Placing").resolve().perform()
+            PlaceAction(object_dict[key], ["left"], [grasp], [Pose([4.9, 2.15, 0.80])]).resolve().perform()
             ParkArmsAction([Arms.LEFT]).resolve().perform()
-            #one = True
-            # MoveGripperMotion(motion="open", gripper="left").resolve().perform()
-            # ParkArmsAction([Arms.LEFT]).resolve().perform()
 
-
-    #Place the cereal at the target location
-    # cereal_target_pose = Pose([4.1, 2.2, 0], object_orientation)
-    # NavigateAction(target_locations=[cereal_target_pose]).resolve().perform()
-    # PlaceAction(dict_desig["breakfast_cereal"], ["left"], ["front"], [Pose([4.8, 2.6, 0.97])]).resolve().perform()
-    #
-    # #Finalize actions
-    # ParkArmsAction([Arms.LEFT]).resolve().perform()
-    #giskardpy.removing_of_objects()
     rospy.loginfo("Done!")
     TalkingMotion("Done").resolve().perform()
+
+
+
 
