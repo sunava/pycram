c52c714e0b7045e3765fa425d7ca8073000c5d2c
Celina SuturoLaptop <celina5@uni-bremen.de>
2024-02-01 13:10:48 +0100

    changed kitchen

--- demos/pycram_suturo/save-urdf.py
+++ demos/pycram_suturo/save-urdf.py
@@ -2,6 +2,6 @@ from urdf_parser_py.urdf import URDF
 
 kitchen = URDF.from_parameter_server('iai_kitchen')
 print (kitchen)
-f = open("../../resources/test-room.urdf", "w")
+f = open("../../resources/couch-kitchen.urdf", "w")
 f.write(kitchen.to_xml_string())
 
