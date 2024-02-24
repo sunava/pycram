6db2913ec742fdfe9012dabcf432b4b4339a65f8
kecks <pkehr@uni-bremen.de>
2024-02-24 02:12:54 +0100

    updated binder structure

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,7 @@ source ${PYCRAM_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rvizweb_config.json &
 roslaunch --wait pycram ik_and_description.launch &
-
+roslaunch --wait pycram giskard_pr2.launch &
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
 
 xvfb-run exec "$@"
