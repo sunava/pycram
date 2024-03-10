c5782bafb3884696fbd978c1076a6efafc585540
kecks <pkehr@uni-bremen.de>
2024-03-10 18:31:26 +0100

    ManualMarker and AxisMarker now properly working

--- src/pycram/ros/viz_marker_publisher.py
+++ src/pycram/ros/viz_marker_publisher.py
@@ -1,8 +1,10 @@
 import atexit
+import logging
 import threading
 import time
+from enum import Enum
 
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3, Quaternion, Point
 from std_msgs.msg import ColorRGBA
 
 from pycram.bullet_world import BulletWorld, Object
@@ -11,7 +13,7 @@ import rospy
 import urdf_parser_py
 from tf.transformations import quaternion_from_euler
 
-from pycram.pose import Transform
+from pycram.pose import Transform, Pose
 
 
 class VizMarkerPublisher:
@@ -110,3 +112,317 @@ class VizMarkerPublisher:
         """
         self.kill_event.set()
         self.thread.join()
+
+
+class ManualMarkerPublisher:
+    def __init__(self, topic_name='/pycram/manual_marker'):
+        """
+        Class to manually add and remove marker of objects and poses.
+
+        :param topic_name: Name of the marker topic
+        """
+        self.marker_array_pub = rospy.Publisher(topic_name, MarkerArray, queue_size=10)
+
+        self.marker_array = MarkerArray()
+        self.marker_overview = {}
+        self.current_id = 0
+
+    def publish(self, pose: Pose, color=None, bulletworld_object=None, name=None):
+        """
+        Publish a pose or an object into the MarkerArray.
+        Priorities to add an object if possible
+
+        :param pose: Pose of the marker
+        :param color: Color of the marker if no object is given
+        :param bulletworld_object: Object to add as a marker
+        :param name: Name of the marker
+        """
+        if color is None:
+            color = 'pink'
+
+        if bulletworld_object is None:
+            self._publish_pose(name=name, pose=pose, color=color)
+        else:
+            self._publish_object(name=name, pose=pose, bw_object=bulletworld_object)
+        rospy.sleep(1)
+
+    def _publish_pose(self, name, pose, color):
+        """
+        Publish a Pose as a marker
+
+        :param name: Name of the marker
+        :param pose: Pose of the marker
+        :param color: Color of the marker
+        """
+
+        if name is None:
+            name = 'pose_marker'
+
+        if name in self.marker_overview.keys():
+            self._update_marker(self.marker_overview[name], new_pose=pose)
+            return
+
+        color_rgba = Colors.get_color(color)
+
+        self.create_marker(name=name, marker_type=Marker.ARROW, marker_pose=pose,
+                           marker_scales=(0.1, 0.01, 0.01), color_rgba=color_rgba)
+        self.marker_array_pub.publish(self.marker_array)
+
+    def _publish_object(self, name, pose, bw_object):
+        """
+        Publish an Object as a marker
+
+        :param name: Name of the marker
+        :param pose: Pose of the marker
+        :param bw_object: Bulletworld object for the marker
+        """
+
+        bw_real = bw_object.resolve()
+
+        if name is None:
+            name = bw_real.name
+
+        if name in self.marker_overview.keys():
+            self._update_marker(self.marker_overview[name], new_pose=pose)
+            return
+
+        path = bw_real.bullet_world_object.urdf_object.links[0].visual.geometry.filename
+
+        self.create_marker(name=name, marker_type=Marker.MESH_RESOURCE, marker_pose=pose,
+                           path_to_resource=path)
+
+        self.marker_array_pub.publish(self.marker_array)
+        rospy.loginfo("Marker array published")
+
+    def create_marker(self, name, marker_type, marker_pose: Pose, marker_scales=(1.0, 1.0, 1.0),
+                      color_rgba=ColorRGBA(1.0, 1.0, 1.0, 1.0), path_to_resource=None):
+        """
+        Create a Marker and add it to the MarkerArray
+
+        :param name: Name of the Marker
+        :param marker_type: Type of the marker to create
+        :param marker_pose: Pose of the marker
+        :param marker_scales: individual scaling of the markers axes
+        :param color_rgba: Color of the marker as RGBA
+        :param path_to_resource: Path to the resource of a Bulletworld object
+        """
+
+        frame_id = marker_pose.header.frame_id
+
+        new_marker = Marker()
+        new_marker.id = self.current_id
+        new_marker.header.frame_id = frame_id
+        new_marker.header.stamp = rospy.Time.now()
+        new_marker.type = marker_type
+        new_marker.action = Marker.ADD
+        new_marker.pose.position.x = marker_pose.pose.position.x
+        new_marker.pose.position.y = marker_pose.pose.position.y
+        new_marker.pose.position.z = marker_pose.pose.position.z
+        new_marker.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)
+        new_marker.scale.x = marker_scales[0]
+        new_marker.scale.y = marker_scales[1]
+        new_marker.scale.z = marker_scales[2]
+        new_marker.color.a = color_rgba.a
+        new_marker.color.r = color_rgba.r
+        new_marker.color.g = color_rgba.g
+        new_marker.color.b = color_rgba.b
+
+        if path_to_resource is not None:
+            new_marker.mesh_resource = 'file://' + path_to_resource
+
+        self.marker_array.markers.append(new_marker)
+        self.marker_overview[name] = new_marker.id
+        self.current_id += 1
+
+    def _update_marker(self, marker_id, new_pose):
+        """
+        Update an existing marker to a new pose
+
+        :param marker_id: id of the marker that should be updated
+        :param new_pose: Pose where the updated marker is set
+        """
+
+        # Find the marker with the specified ID
+        for marker in self.marker_array.markers:
+            if marker.id == marker_id:
+                # Update successful
+                marker.pose = new_pose
+                rospy.loginfo(f"Marker {marker_id} updated")
+                self.marker_array_pub.publish(self.marker_array)
+                return True
+
+        # Update was not successful
+        rospy.logwarn(f"Marker {marker_id} not found for update")
+        return False
+
+    def remove_marker(self, bw_object=None, name=None):
+        """
+        Remove a marker by object or name
+
+        :param bw_object: Object which marker should be removed
+        :param name: Name of object that should be removed
+        """
+
+        if bw_object is not None:
+            bw_real = bw_object.resolve()
+            name = bw_real.name
+
+        if name is None:
+            logging.error('No name for object')
+            return
+
+        marker_id = self.marker_overview.pop(name)
+
+        for marker in self.marker_array.markers:
+            if marker.id == marker_id:
+                marker.action = Marker.DELETE
+
+        self.marker_array_pub.publish(self.marker_array)
+        self.marker_array.markers.pop(marker_id)
+        self.marker_array_pub.publish(self.marker_array)
+
+    def clear_all_marker(self):
+        """
+        Clear all existing markers
+        """
+        for marker in self.marker_array.markers:
+            marker.action = Marker.DELETE
+
+        self.marker_overview = {}
+        self.marker_array_pub.publish(self.marker_array)
+
+
+class AxisMarkerPublisher:
+    def __init__(self, topic='/pycram/axis_marker', marker_id=0, frame_id='map'):
+        self.topic = topic
+        self.marker_id = marker_id
+        self.frame_id = frame_id
+        self.marker_pub = rospy.Publisher(self.topic, MarkerArray, queue_size=10)
+        self.marker_array = MarkerArray()
+        self.marker_pub.publish(self.marker_array)
+
+    def publish(self, pose, axis, duration=5.0, length=2.0, color=None):
+        """
+        Publish a MarkerArray with given pose and axis.
+        Duration, length and color of the line are optional.
+
+        :param pose: Starting position of the Line
+        :param axis: Orientation for the Line
+        :param duration: Duration of the marker
+        :param length: Length of the line
+        :param color: Color of the line if it should be personalized
+        """
+
+        if isinstance(axis, AxisIdentifier):
+            axis = axis.value
+
+        if color is None:
+            color = self._get_color(axis)
+        else:
+            color = Colors.get_color(color)
+
+        self._create_line(pose, axis, duration, length, color)
+
+        rospy.sleep(1)
+
+        print(self.marker_array)
+
+        # Publish the MarkerArray
+        self.marker_pub.publish(self.marker_array)
+
+    def _get_color(self, axis):
+        """
+        Get the color of the given Axis
+
+        :param axis: Used axis
+        """
+        if axis == AxisIdentifier.X.value:
+            axis_color = Colors.get_color('red')
+        elif axis == AxisIdentifier.Y.value:
+            axis_color = Colors.get_color('green')
+        elif axis == AxisIdentifier.Z.value:
+            axis_color = Colors.get_color('blue')
+        else:
+            logging.error(f'Axis {str(axis)} is not valid')
+            axis_color = Colors.get_color('red')
+
+        return axis_color
+
+    def _create_line(self, pose, axis, duration, length, color):
+        """
+        Create a line marker to add to the marker array.
+
+        :param pose: Starting pose of the line
+        :param axis: Axis along which the line is set
+        :param duration: Duration of the line marker
+        :param length: Length of the line
+        :param color: Optional color for the Line
+        """
+
+        # Create a line marker for the axis
+        line_marker = Marker()
+        line_marker.header.frame_id = self.frame_id
+        line_marker.header.stamp = rospy.Time.now()
+        line_marker.ns = 'axis_visualization'
+        line_marker.id = self.marker_id
+        line_marker.type = Marker.LINE_LIST
+        line_marker.action = Marker.ADD
+        line_marker.pose = pose
+        line_marker.scale.x = 0.01  # Line width
+        line_marker.color = color
+        line_marker.lifetime = rospy.Duration(duration)
+
+        # Create two points for the line (start and end)
+        start_point = Point()
+        start_point.x = pose.position.x
+        start_point.y = pose.position.y
+        start_point.z = pose.position.z
+
+        end_point = Point()
+        end_point.x = pose.position.x + (axis[0] * length)
+        end_point.y = pose.position.y + (axis[1] * length)
+        end_point.z = pose.position.z + (axis[2] * length)
+
+        line_marker.points.append(start_point)
+        line_marker.points.append(end_point)
+
+        # Add the line marker to the MarkerArray
+        self.marker_array.markers.append(line_marker)
+
+
+class AxisIdentifier(Enum):
+    X = (1, 0, 0)
+    Y = (0, 1, 0)
+    Z = (0, 0, 1)
+
+
+class Colors:
+    @staticmethod
+    def get_color(color):
+        """
+        Retrieve a color based on name
+
+        :param color: Name of a color
+        """
+
+        converted_color = ColorRGBA()
+        value = lambda x: x / 255
+
+        if color == 'pink':
+            return ColorRGBA(r=value(255), g=value(0), b=value(255), a=1.0)
+        elif color == 'black':
+            return ColorRGBA(r=value(0), g=value(0), b=value(0), a=1.0)
+        elif color == 'white':
+            return ColorRGBA(r=value(255), g=value(255), b=value(255), a=1.0)
+        elif color == 'red':
+            return ColorRGBA(r=value(255), g=value(0), b=value(0), a=1.0)
+        elif color == 'green':
+            return ColorRGBA(r=value(0), g=value(255), b=value(0), a=1.0)
+        elif color == 'blue':
+            return ColorRGBA(r=value(0), g=value(0), b=value(255), a=1.0)
+        elif color == 'yellow':
+            return ColorRGBA(r=value(255), g=value(255), b=value(0), a=1.0)
+        elif color == 'cyan':
+            return ColorRGBA(r=value(0), g=value(255), b=value(255), a=1.0)
+
+        return converted_color
