afabf6564e33d6f192456b7026d869bbe109230d
Luca Krohm <luc_kro@uni-bremen.de>
2024-03-25 16:49:19 +0100

    [binder] restore original binder folder

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,7 +3,7 @@
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rvizweb_config.json &
-roslaunch --wait pycram ik_and_description.launch robot:=ARMAR6 &
+roslaunch --wait pycram ik_and_description.launch &
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
 
 xvfb-run exec "$@"
