0cf75907b5966ef275df47d8b24b508fe482d166
OEM Configuration (temporary user) <hassouna@uni-bremen.de>
2024-04-23 21:04:25 +0200

    [rk external] interface changes

--- src/pycram/external_interfaces/robokudo.py
+++ src/pycram/external_interfaces/robokudo.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any
 
 from geometry_msgs.msg import PoseStamped
 from typing_extensions import Callable
@@ -11,7 +12,7 @@ from ..designator import ObjectDesignatorDescription
 from pycram.datastructures.pose import Pose
 from pycram.local_transformer import LocalTransformer
 from pycram.world import World
-from pycram.datastructures.enums import ObjectType
+from pycram.datastructures.enums import ObjectType, PerceiveTechnique
 from ..plan_failures import PerceptionLowLevelFailure
 
 try:
@@ -57,7 +58,7 @@ def create_robokudo_action_client() -> Callable:
     rospy.loginfo("Waiting for action server")
     client.wait_for_server()
 
-    def action_client(object_desc):
+    def action_client(technique, object_type=None, object_color=None):
         global query_result
 
         def active_callback():
@@ -71,7 +72,7 @@ def create_robokudo_action_client() -> Callable:
         def feedback_callback(msg):
             pass
 
-        object_goal = make_query_goal_msg(object_desc)
+        object_goal = make_query_goal_msg(technique, object_type, object_color)
         client.send_goal(object_goal, active_cb=active_callback, done_cb=done_callback, feedback_cb=feedback_callback)
         wait = client.wait_for_result()
         return query_result
@@ -93,39 +94,70 @@ def msg_from_obj_desig(obj_desc: ObjectDesignatorDescription) -> 'robokudo_Objet
     return obj_msg
 
 
-def make_query_goal_msg(technique, object_type=None, object_color=None) -> 'QueryGoal':
+def make_query_goal_msg(
+    technique=None, object_type=None, object_color=None, object_shape=None, object_location=None
+) -> "QueryGoal":
     """
-    Generates a QueryGoal message from a specific technique that describes what needs to be perceived for querying
-    RobotKudo. The default message is empty and gets filled with the given parameters. However, if the technique is
-    not 'human', 'color', or 'type', it will result in a broader perception where all objects are considered.
+    Generates a QueryGoal message from a specific technique that describes what needs to be perceived for querying RobotKudo.
+    If the technique is not 'human', 'color', 'type', 'shape', or 'location', the perception will be broader,
+    considering all objects.
 
-    :param technique: The technique that should be used for the query, e.g. type, color, human
-    :param object_type: The object type that should be perceived
-    :param object_color: The object color that should be perceived
-    :return: The RoboKudo QueryGoal message
+    :param technique: The technique for the query, e.g. 'type', 'color', 'human', 'shape', 'location', 'specific'.
+    :param object_type: The object type to be perceived (required for 'type' technique).
+    :param object_color: The object color to be perceived (required for 'color' technique).
+    :param object_shape: The object shape to be perceived (required for 'shape' technique).
+    :param object_location: The object location to be perceived (required for 'location' technique).
+    :return: A QueryGoal message.
     """
     goal_msg = QueryGoal()
-    if technique == "type":
+
+    if technique == PerceiveTechnique.TYPE:
         if not object_type:
             raise PerceptionLowLevelFailure(
-                f"For technique='Type' the object_type must be set, but is {object_type}")
+                f"For technique 'type', 'object_type' must be set, but it's {object_type}"
+            )
+        goal_msg.obj.type = str(object_type)
 
-        goal_msg.obj.obj_type = str(object_type)
-
-    elif technique == "color":
+    elif technique == PerceiveTechnique.COLOR:
         if not object_color:
             raise PerceptionLowLevelFailure(
-                f"For technique='color' the object_color must be set, but is {object_color}")
+                f"For technique 'color', 'object_color' must be set, but it's {object_color}"
+            )
         goal_msg.obj.color.append(str(object_color))
 
-    elif technique == 'human':
-        goal_msg.obj.obj_type = "human"
+    elif technique == PerceiveTechnique.HUMAN:
+        goal_msg.obj.type = "human"
+
+    elif technique == PerceiveTechnique.SHAPE:
+        if not object_shape:
+            raise PerceptionLowLevelFailure(
+                f"For technique 'shape', 'object_shape' must be set, but it's {object_shape}"
+            )
+        goal_msg.obj.shape = str(object_shape)
+
+    elif technique == PerceiveTechnique.LOCATION:
+        if not object_location:
+            raise PerceptionLowLevelFailure(
+                f"For technique 'location', 'object_location' must be set, but it's {object_location}"
+            )
+        goal_msg.obj.location = str(object_location)
+
+    elif technique == PerceiveTechnique.SPECIFIC:
+        if object_type:
+            goal_msg.obj = msg_from_obj_desig(object_type)
+        elif object_color:
+            goal_msg.obj.color.append(str(object_color))
+        elif object_shape:
+            goal_msg.obj.shape = str(object_shape)
+        elif object_location:
+            goal_msg.obj.location = str(object_location)
 
     return goal_msg
 
 
+
 @init_robokudo_interface
-def query(object_desc: ObjectDesignatorDescription) -> ObjectDesignatorDescription.Object:
+def query(technique, object_type=None, object_color=None) -> Any:
     """
     Sends a query to RoboKudo to look for an object that fits the description given by the Object designator description.
     For sending the query to RoboKudo a simple action client will be created and the Object designator description is
@@ -134,23 +166,23 @@ def query(object_desc: ObjectDesignatorDescription) -> ObjectDesignatorDescripti
     :param object_desc: The object designator description which describes the object that should be perceived
     :return: An object designator for the found object, if there was an object that fitted the description.
     """
-    query_result = robokudo_action_client(object_desc)
+    query_result = robokudo_action_client(technique, object_type, object_color)
     pose_candidates = {}
-    if query_result.res == []:
+    if not query_result.res:
         rospy.logwarn("No suitable object could be found")
         return
 
-    for i in range(0, len(query_result.res[0].pose)):
-        pose = Pose.from_pose_stamped(query_result.res[0].pose[i])
-        pose.frame = World.current_world.robot.get_link_tf_frame(pose.frame)  # TODO: pose.frame is a link name?
-        source = query_result.res[0].poseSource[i]
+    # for i in range(0, len(query_result.res[0].pose)):
+    #     pose = Pose.from_pose_stamped(query_result.res[0].pose[i])
+    #     pose.frame = World.current_world.robot.get_link_tf_frame(pose.frame)  # TODO: pose.frame is a link name?
+    #     source = query_result.res[0].poseSource[i]
+    #
+    #     lt = LocalTransformer()
+    #     pose = lt.transform_pose(pose, "map")
+    #
+    #     pose_candidates[source] = pose
 
-        lt = LocalTransformer()
-        pose = lt.transform_pose(pose, "map")
-
-        pose_candidates[source] = pose
-
-    return pose_candidates
+    return query_result.res
 
 @init_robokudo_interface
 def query_empty(object_desc: ObjectDesignatorDescription):
