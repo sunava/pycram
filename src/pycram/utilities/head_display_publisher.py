c609b2b2ef55e7e4956ff867abc554eca596a6a3
kecks <pkehr@uni-bremen.de>
2024-04-25 17:06:46 +0200

    [hsrb_display]: added documentation of head_display_publisher

--- src/pycram/utilities/head_display_publisher.py
+++ src/pycram/utilities/head_display_publisher.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 
 import rospy
 import cv2
@@ -9,8 +10,16 @@ from sensor_msgs.msg import Image
 
 
 class HeadDisplayPublisher:
+    """
+    Publisher to send locally available images to the head display of the hsrb
+    """
 
     def __init__(self, topic="/head_display/receiver"):
+        """
+        Create a publisher for images
+
+        :param topic: The topic to publish images to. Default is the same as on the visualizer script of the hsrb.
+        """
         self.stop_thread = False
         self.thread = None
         self.bridge = CvBridge()
@@ -21,24 +30,30 @@ class HeadDisplayPublisher:
         # Create a publisher for the image
         self.publisher = rospy.Publisher(topic, Image, queue_size=10)
 
-        print("publisher started")
+        logging.info("Publisher started")
 
-    def publish_image(self, path, publish_constantly=True, frequency=0.3):
+    def publish_image(self, path, frequency=0.3):
+        """
+        Publish an image constantly to the given ros topic.
+
+        :param path: Path of the picture to send.
+        :param frequency: How often the image should be sent again
+        """
         self.image_path = path
         self.frequency = frequency
 
-        if publish_constantly:
-            self.thread = Thread(target=self._pub)
-            self.thread.start()
-        else:
-            self._pub()
+        self.thread = Thread(target=self._pub)
+        self.thread.start()
 
     def stop_publishing(self):
+        """
+        Stop the thread that publishes an image.
+        """
         if self.thread.is_alive():
             self.stop_thread = True
             self.thread.join()
         else:
-            rospy.logwarn("Thread is not running")
+            logging.warning("No thread for publishing images is running")
 
     def _pub(self):
         image = cv2.imread(self.image_path)
@@ -51,7 +66,7 @@ class HeadDisplayPublisher:
             while not self.stop_thread:
                 # Publish the ROS image message
                 self.publisher.publish(ros_image)
-                rospy.loginfo("published image for head display")
+                logging.debug("Published image for head display")
                 rospy.sleep(self.frequency)
         else:
-            rospy.logerr("Failed to load image: %s", self.image_path)
+            logging.error("Failed to load image: %s", self.image_path)
