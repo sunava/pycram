e28e203c1ef3721317f945d9554f8681a7741b75
Celina SuturoLaptop <celina5@uni-bremen.de>
2024-06-06 17:18:35 +0200

    [Stroing groceries] dictionary for objects, sorting result from perception, class for shelf compartment

--- demos/pycram_storing_groceries_demo/utils/misc.py
+++ demos/pycram_storing_groceries_demo/utils/misc.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from demos.pycram_storing_groceries_demo.utils.misc import *
 from docutils.nodes import math
 from pycram.designators.action_designator import *
@@ -9,6 +11,24 @@ from pycram.designators.object_designator import *
 pose_test_area = Pose([2.25, 2.4, 0], [0, 0, 0, 1])
 pose_cabinet = Pose([2.25, 2.4, 0], [0, 0, 0, 1])
 pose_table = Pose([1.5, 2.0, 0], [0, 0, 1, 0])
+
+categories = {
+    "Sports Equipment": ["Minisoccerball", "Baseball", "Softball", "Tennisball"],
+    "Cutlery": ["Fork", "Spoon", "Knife", "Metalplate", "Metalbowl"],
+    "Drinkware": ["Pitcher", "Wineglass", "Metalmug", "Cupblue", "Cupgreen", "Cup_small"],
+    "Cleaning Supplies": ["Bleachcleanserbottle", "Glasscleanerspraybottle", "Abrasivesponge", "Scrubcleaner", "Dishwashertab"],
+    "Tools": ["Scissors", "Screwdriver", "Clamp", "Hammer", "Wooden_block"],
+    "Toys/Games": ["Rubikscube"],
+    "Other": ["Largemarker"],
+    "Snacks": ["Crackerbox", "Pringleschipscan"],
+    "Breakfast Foods": ["Cerealbox", "Mueslibox", "Cornybox"],
+    "Canned Goods": ["Tomatosoupcan", "Tunafishcan", "Pottedmeatcan", "Masterchefcan"],
+    "Beverage Ingredients": ["Coffeecan", "Coffeepack", "Sugarbox", "Milkpackja"],
+    "Condiments": ["Mustardbottle"],
+    "Fruits": ["Orange", "Strawberry", "Apple", "Pear", "Lemon", "Banana", "Peach", "Plum", "Grapes"]
+}
+
+item_to_category = {item: category for category, items in categories.items() for item in items}
 
 
 def navigate_and_detect(location: str):
@@ -35,38 +55,67 @@ def navigate_and_detect(location: str):
     return object_desig
 
 
-def sort_obj(obj_dict: dict, robot: BulletWorldObject):
-    """
-    sort objects perceived so that knowledge can work with list
+def sort_new(perceived: dict, robot: BulletWorldObject):
+    obj_list = {}
+    sorted_obj_list = []
 
-    :return: list for knowledge call
-    """
-
-    sorted_objects = []
-    obj_list = []
-
-    if len(obj_dict) == 0:
-        return sorted_objects
-
-    # cut of the given State and keep the dictionary
-    first, *remaining = obj_dict
+    obj_dict = perceived[1]
     robot_pose = robot.get_pose()
-    # calculate euclidian distance for all found object in a dictionary
-    for dictionary in remaining:
-        for value in dictionary.values():
-            distance = math.sqrt(pow((value.pose.position.x - robot_pose.pose.position.x), 2) +
-                                 pow((value.pose.position.y - robot_pose.pose.position.y), 2) +
-                                 pow((value.pose.position.z - robot_pose.pose.position.z), 2))
+    for value in obj_dict:
+        # calculate distance
+        distance = math.sqrt(pow((obj_dict[value].pose.position.x - robot_pose.pose.position.x), 2) +
+                             pow((obj_dict[value].pose.position.y - robot_pose.pose.position.y), 2) +
+                             pow((obj_dict[value].pose.position.z - robot_pose.pose.position.z), 2))
 
-            # fill dict with objects and their distance.
-            # Add seen objects only once, if seen multiple times
-            if value.type not in obj_list:
-                obj_list.append((value, distance))
+        # get position of object
+        obj_pose = Pose()
+        obj_pose.position = [obj_dict[value].pose.position.x, obj_dict[value].pose.position.y,
+                             obj_dict[value].pose.position.z]
+        obj_pose.orientation = [obj_dict[value].pose.orientation.x, obj_dict[value].pose.orientation.y,
+                                obj_dict[value].pose.orientation.z, obj_dict[value].pose.orientation.w]
 
-    # sort all objects that where in the obj_dict
-    sorted_objects = sorted(obj_list, key=lambda distance: distance[1])
 
-    return [a for (a, _) in sorted_objects]
+        # put everything in a dict
+        obj_list[value] = obj_pose
+        sorted_obj_list.append((obj_dict[value].type, distance))
+
+    sorted_obj_list = sorted(sorted_obj_list, key=lambda distance: distance[1])
+
+    return [sorted_obj_list, obj_list]
+
+
+def sort_and_remove_duplicates(unsorted_list):
+    #letters to lowercase
+    unsorted_list = [s.lower() for s in unsorted_list]
+
+    #count frequency of each string
+    frequency = Counter(unsorted_list)
+
+    #remove duplicates (transform to set and back to list)
+    unique_strings = list(set(unsorted_list))
+
+    #sort list according to frequency
+    sorted_unique_strings = sorted(unique_strings, key=lambda x: (-frequency[x], x))
+
+    return sorted_unique_strings
+
+
+def order_categorys(objects:List):
+    categorys = []
+    for obj in objects:
+        try:
+            categorys.append(item_to_category[obj])
+        except KeyError:
+            categorys.append("unknown")
+
+    ordered_categorys = sort_and_remove_duplicates(categorys)
+    return ordered_categorys
+
+def get_category(obj: str):
+    try:
+        return item_to_category[obj]
+    except KeyError:
+        return "unknown"
 
 
 
--- src/pycram/designators/object_designator.py
+++ src/pycram/designators/object_designator.py
@@ -245,3 +245,53 @@ class HumanDescription:
         :param attribute_list: list with attributes: gender, headgear, kind of clothes, bright/dark clothes
         """
         self.attributes = attribute_list[1]
+
+
+class ShelfCompartmentDescription:
+    """
+    Class that represents a Compartment in a shelf but in cool and convenient
+    """
+    def __init__(self, height: float, placing_areas: List[List[float]], category: Optional[List] = None):
+        """
+        :param height: height of compartment
+        :param placing_areas: x/y-coordinate of possible placing range [x min, x max]
+        :param category: category of object already standing in the compartment
+        """
+
+        self.height = height
+        self.placing_areas = placing_areas
+        self.category = category
+
+        # list that tracks if area x is occupied
+        # we assume that the compartment is empty, therefore everything is set to False
+        area_free = []
+        for i in range(len(placing_areas)):
+            area_free.append(False)
+
+        def set_area_occupied(area: int, occ: bool):
+            area_free[area] = occ
+
+        def get_area_occupied(area: int):
+            return area_free[area]
+
+        def get_free_area():
+            for area in range(len(placing_areas)):
+                if not area_free[area]:
+                    # return arithmetic mean of area
+                    return (placing_areas[area][0] + placing_areas[area][1])/2
+            return -1
+
+        def get_placing_pose(obj_category: str):
+            for cat in category:
+                if cat == obj_category:
+                    placing_pose = get_free_area()
+                    if placing_pose != -1:
+                        return placing_pose
+                    else:
+                        return -1
+            return -1
+
+
+
+
+
