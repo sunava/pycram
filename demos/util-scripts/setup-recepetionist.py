cfd093a3a4e40ee664c9be5524a28e6033c8a015
juschulz <juschulz@uni-bremen.de>
2024-07-05 11:15:17 +0200

    [Receptionist] added changes from Vanessa, comments on setup

--- demos/util-scripts/setup-recepetionist.py
+++ demos/util-scripts/setup-recepetionist.py
@@ -34,23 +34,35 @@ run_command(f'gnome-terminal -- bash -c "tmux new-session -d -s {session_name}; 
             f'tmux split-window -v -t {session_name}:0.5; '
             f'tmux split-window -v -t {session_name}:0.6; '
             f'tmux split-window -v -t {session_name}:0.7; '
+            # Terminal1 0: Rviz map launch 
             f'tmux send-keys -t {session_name}:0.0 \'roslaunch suturo_bringup envi_bringup.launch\' C-m; '
+            
+            # Terminal1 1: Giskard launch
             f'tmux send-keys -t {session_name}:0.1 \'roslaunch giskardpy giskardpy_hsr_real_vel.launch\' C-m; '
+            
+            # Terminal1 2: Perception launch
             f'tmux send-keys -t {session_name}:0.2 \'robokudo_venv; rosrun robokudo main.py _ae=receptionist _ros_pkg=milestone1\' C-m; '
+           
+            # Terminal1 3: HSR Display launch
             f'tmux send-keys -t {session_name}:0.3 \'sshpass -p hsrhmi ssh -t hsr-hmi@hsrb.local\' C-m; '
             f'tmux send-keys -t {session_name}:0.3 \'wait && ./launch_display.sh\' C-m; '
             
+            # Terminal 4: Rasa start and Audio topic check
             f'tmux send-keys -t {session_name}:0.4 \'virtual_nlp\' C-m; '
             f'tmux send-keys -t {session_name}:0.4 \'cd /home/suturo/suturo23_24/nlp_ws/src/Rasa/rasa \' C-m; '
             f'tmux send-keys -t {session_name}:0.4 \'rasa run --enable-api & \' C-m; '
             f'tmux send-keys -t {session_name}:0.4 \'rostopic hz /audio/audio\' C-m; '
             
-
+            # Terminal1 5: NLP Bell-Detection script start
             f'tmux send-keys -t {session_name}:0.5 \'rosrun sound_detection nlp_sound_detect.py  \' C-m; '
+            
+            # Terminal1 6: NLP HRI-Script start
             f'tmux send-keys -t {session_name}:0.6 \'nlp_venv; cd /home/suturo/suturo23_24/nlp_ws/src/suturo_nlp/activate_language_processing/scripts ; python3 nlp_receptionist.py -hsr\' C-m; '
+            
+            # Terminal1 7: HSR Audio topic  start
             f'tmux send-keys -t {session_name}:0.7 \'sshpass -p password ssh -t administrator@hsrb.local\' C-m; '
             f'tmux send-keys -t {session_name}:0.7 \'wait && roslaunch audio_capture capture_wave.launch\' C-m; '
-            # f'tmux send-keys -t {session_name}:0.8 \'rostopic hz /audio/audio\' C-m; '
+
             f'tmux attach-session -t {session_name}"')
 
 # Keep the script running to handle signals
