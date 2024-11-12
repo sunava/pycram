ab070fa3e44ffc39fadaf2e25eec65224166a4dc
Vanessa Hassouna <33067562+sunava@users.noreply.github.com>
2024-11-12 11:01:59 -0500

    [fallschool] day3 mongodb

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+# Launch ros web applications
+source ${ROS_WS}/devel/setup.bash
+roscore &
+roslaunch --wait rvizweb rvizweb.launch &
 
-source ${PYCRAM_WS}/devel/setup.bash
 # Start MongoDB and save data on working directory
 MONGODB_URL=mongodb://127.0.0.1:27017
 # Store MongoDB data under directory ${HOME}/data/db
 mongod --fork --logpath ${HOME}/mongod.log
-roscore &
-roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
-roslaunch --wait pycram ik_and_description.launch robot:='pr2'&
-roslaunch --wait suturo_knowledge suturo_knowledge.launch &
-roslaunch --wait knowrob_designator knowrob_designator_service.launch &
 
-cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json
+# Launch Knowrob
+export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
+roslaunch --wait knowrob knowrob.launch &
 
-xvfb-run exec "$@"
+exec "$@"
