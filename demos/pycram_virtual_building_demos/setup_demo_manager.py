9bfb90b769f04efec3a0a8082c741134151d4efd
sunava <hassouna@uni-bremen.de>
2025-02-13 17:56:28 -0500

    stop local

--- demos/pycram_virtual_building_demos/setup_demo_manager.py
+++ demos/pycram_virtual_building_demos/setup_demo_manager.py
@@ -110,4 +110,4 @@ def demo_selecting(envi, robot, task_param):
         specialized_task = rospy.get_param('/nbparam_specialized_task')
         start_generalized_demo(task_param, object_tool, object_target, specialized_task)
 
-start_demo_local()
+# start_demo_local()
