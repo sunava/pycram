d48425725f0881aed6710aaf5b2e9b4c68671d86
sunava <hassouna@uni-bremen.de>
2025-06-10 10:24:27 +0200

    merged base branch

--- src/pycram/process_modules/default_process_modules.py
+++ src/pycram/process_modules/default_process_modules.py
@@ -433,7 +433,8 @@ class DefaultMoveTCPReal(ProcessModule):
         pose_in_map = lt.transform_pose(designator.target, "map")
 
         tip_link = RobotDescription.current_robot_description.get_arm_chain(designator.arm).get_tool_frame()
-        root_link = RobotDescription.current_robot_description.base_link
+
+        root_link = "map"
 
         gripper_that_can_collide = designator.arm if designator.allow_gripper_collision else None
         if designator.allow_gripper_collision:
